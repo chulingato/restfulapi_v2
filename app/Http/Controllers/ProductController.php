fb8bbdb2c82db455a868f6c3157f1e4cfae160ab
chulingato <knx3.marquez@gmail.com>
2021-09-28 08:37:10 -1200

    add a create method to products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,8 +9,45 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
+    
         return Product::all();
-
+        
+        /*$product = Product::all();
+        foreach ($product as $products) {
+                return response()->json([
+                'Producto' => [
+                    'nombre'=>$products->name,
+                    'stock'=>$products->id 
+                ]
+            ], 201);
+        }*/
 
     }
+
+    public function create(Request $request)
+    {
+          if ($request->input('type')==='crear') {
+       return $this->createProduct(
+        $request->input('nombre'),
+        $request->input('stock')
+       );
+        }
+
+    }//end functino create
+
+    private function createProduct($name, $stock)
+{
+    $product = Product::firstOrCreate(
+    ['name' => $name],
+    ['stock' => $stock]
+);
+
+    return response()->json([
+                'producto' => [
+                    'name'=>$product->name,
+                    'stock'=>$product->stock
+                ]
+            ], 201);
+
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::get('/product','App\Http\Controllers\ProductController@index');
+Route::post('/product','App\Http\Controllers\ProductController@create');
